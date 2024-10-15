99e7ff6d2d8d58be7e72263540ce5bb0c2acf26c
Ashadowmonarch <randomtry4@gmail.com>
2024-10-14 21:29:59 -0400

    Add changes to account page

--- frontend/screens/AccountScreen.jsx
+++ frontend/screens/AccountScreen.jsx
@@ -1,12 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 const AccountScreen = () => {
   return (
-    <View>
+    <View style={styles.accountScreen}>
       <Text>AccountScreen</Text>
     </View>
   );
 };
 
 export default AccountScreen;
+
+const styles = StyleSheet.create({
+  accountScreen: {
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#2272BE",
+  },
+});
